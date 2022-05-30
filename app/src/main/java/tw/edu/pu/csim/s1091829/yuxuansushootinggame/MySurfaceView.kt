b11c4b591c6467aa5e7b59633d031d25173e244b
Su <s1091829@gm.pu.edu.tw>
2022-05-30 20:54:38 +0800

    v.4 飛行員飛行及上下拖曳

--- app/src/main/java/tw/edu/pu/csim/s1091829/yuxuansushootinggame/MySurfaceView.kt
+++ app/src/main/java/tw/edu/pu/csim/s1091829/yuxuansushootinggame/MySurfaceView.kt
@@ -5,21 +5,29 @@ import android.graphics.*
 import android.media.MediaPlayer
 import android.util.AttributeSet
 import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 
-class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),SurfaceHolder.Callback {
+class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
+    SurfaceHolder.Callback, GestureDetector.OnGestureListener{
 
     var surfaceHolder: SurfaceHolder
     var BG: Bitmap
 
     var BGmoveX:Int = 0
 
+    var fly:Fly
+    var gDetector: GestureDetector
+
     init {
         surfaceHolder = getHolder()
         BG = BitmapFactory.decodeResource(getResources(), R.drawable.background)
 
         surfaceHolder.addCallback(this)
+
+        fly = Fly(context!!)
+        gDetector = GestureDetector(context, this)
     }
 
 
@@ -58,6 +66,8 @@ class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(conte
         paint.textSize = 50f
         canvas.drawText("射擊遊戲(作者：蘇鈺暄)",50f,50f, paint)
 
+        fly.draw(canvas)
+
     }
 
 
@@ -68,6 +78,36 @@ class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(conte
 
     override fun surfaceDestroyed(p0: SurfaceHolder) {
 
+    }
+
+    override fun onDown(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onShowPress(p0: MotionEvent?) {
+
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, p2: Float, p3: Float): Boolean {
+        fly.y = e2!!.y.toInt() - fly.h/2
+        return true
+    }
+
+    override fun onLongPress(p0: MotionEvent?) {
+
+    }
+
+    override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return true
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        gDetector.onTouchEvent(event)
+        return true
     }
 
 }
