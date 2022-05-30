ecd4e57791b4fcfe8e11045ced69db010c574882
Su <s1091829@gm.pu.edu.tw>
2022-05-30 17:27:42 +0800

    v.3 Glide與Chain

--- app/src/main/java/tw/edu/pu/csim/s1091829/yuxuansushootinggame/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1091829/yuxuansushootinggame/MainActivity.kt
@@ -1,11 +1,17 @@
 package tw.edu.pu.csim.s1091829.yuxuansushootinggame
 
+
 import android.content.pm.ActivityInfo
 import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.coroutines.*
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var game : MySurfaceView
     var flag:Boolean = false
     lateinit var job : Job
+    lateinit var imgAuthor : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +30,14 @@ class MainActivity : AppCompatActivity() {
 
         img = findViewById(R.id.img)
         game = findViewById(R.id.game)
+
+        imgAuthor = findViewById(R.id.imgAuthor)
+        GlideApp.with(this)
+            //.load(R.drawable.earth)
+            .load(R.drawable.yxsu)
+            .circleCrop()
+            .override(800, 600)
+            .into(imgAuthor)
 
         img.setOnClickListener({
             if (flag){
